1533826c42b02ea26ec0864676b6d6ac7a9609c3
PolarisJunior <brownasianpj@yahoo.com>
2019-03-02 21:08:27 -0800

    rect io

--- collection/math/geometry/Rect.h
+++ collection/math/geometry/Rect.h
@@ -1,10 +1,19 @@
 
 #pragma once
 #include <cstdint>
+#include <ostream>
 
 struct Rect {
   int32_t x;
   int32_t y;
   int32_t width;
   int32_t height;
+
+  friend std::ostream& operator<<(std::ostream& os, const Rect& rect);
 };
+
+inline std::ostream& operator<<(std::ostream& os, const Rect& rect) {
+  os << "(" << rect.x << ", " << rect.y << ", " << rect.width << ", "
+     << rect.height << ")";
+  return os;
+}
