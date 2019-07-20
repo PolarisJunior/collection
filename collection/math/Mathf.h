7e33895ae0b2edfff1eda2feefda45c3dc6111c8
PolarisJunior <brownasianpj@yahoo.com>
2019-07-20 02:33:35 -0700

    mathf

---
+++ collection/math/Mathf.h
@@ -0,0 +1,8 @@
+#pragma once
+
+struct Mathf {
+  static constexpr float pi = 3.1415926f;
+  static constexpr float pi_4 = pi / 4.0f;
+  static constexpr float deg2Rad = 2.f * pi / 360.f;
+  static constexpr float rad2Deg = 180.f / pi;
+};
