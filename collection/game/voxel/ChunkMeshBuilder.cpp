ee915cd1d5eee159e6e2b562287201cc69e02e98
PolarisJunior <brownasianpj@yahoo.com>
2019-07-24 23:50:46 -0700

    cleaner algorithm, full culling for chunks

--- collection/game/voxel/ChunkMeshBuilder.cpp
+++ collection/game/voxel/ChunkMeshBuilder.cpp
@@ -25,49 +25,53 @@ std::vector<Vector3<int32_t>> getAdjacentPoints(const Vector3<int32_t>& v) {
   return points;
 }
 
-Mesh ChunkMeshBuilder::buildMesh(const Chunk& c) {
+using vec = Vector3<int32_t>;
+std::vector<Vector3<int32_t>> allDirections = {vec::up(),      vec::down(),
+                                               vec::right(),   vec::left(),
+                                               vec::forward(), vec::backward()};
+
+Mesh ChunkMeshBuilder::buildMesh(const Chunk& chunk) {
   Mesh mesh;
   Mesh faceMesh = PlaneMesh();
 
+  std::vector<Mesh> faces;
+  std::fill_n(std::back_inserter(faces), 6, faceMesh);
+
   // faces are with respect to us being -x, -y, -z of the chunk.
-  Mesh topFace = faceMesh;
-  Mesh bottomFace = faceMesh;
+  Mesh& topFace = faces[0];
+  Mesh& bottomFace = faces[1];
   bottomFace.rotateVertices(Quaternion(Mathf::pi, Vec3::forward()));
 
-  Mesh rightFace = faceMesh;
-  Mesh leftFace = faceMesh;
+  Mesh& rightFace = faces[2];
+  Mesh& leftFace = faces[3];
   rightFace.rotateVertices(Quaternion(-Mathf::pi_2, Vec3::right()) *
                            Quaternion(-Mathf::pi_2, Vec3::forward()));
   leftFace.rotateVertices(Quaternion(-Mathf::pi_2, Vec3::right()) *
                           Quaternion(Mathf::pi_2, Vec3::forward()));
 
-  Mesh backFace = faceMesh;
-  Mesh frontFace = faceMesh;
+  Mesh& backFace = faces[4];
+  Mesh& frontFace = faces[5];
+  frontFace.rotateVertices(Quaternion(-Mathf::pi_2, Vec3::right()));
   backFace.rotateVertices(Quaternion(Mathf::pi, Vec3::up()) *
                           Quaternion(-Mathf::pi_2, Vec3::right()));
-  frontFace.rotateVertices(Quaternion(-Mathf::pi_2, Vec3::right()));
 
-  for (auto it = c.blocksInChunk.begin(); it != c.blocksInChunk.end(); ++it) {
+  for (auto it = chunk.blocksInChunk.begin(); it != chunk.blocksInChunk.end();
+       ++it) {
     const Vector3<int32_t> pos = it->first;
     const Block::Type type = it->second;
 
     auto points = getAdjacentPoints(pos);
-    for (auto pIt = points.begin(); pIt != points.end(); ++pIt) {
-      if (c.getBlockType(*pIt) == Block::Type::AIR) {
-        Mesh m;
-        // top and bottom faces
-        mesh.joinMesh(topFace, pos.x, pos.y + 1, pos.z);
-        mesh.joinMesh(bottomFace, pos.x, pos.y, pos.z);
 
-        // front and back faces
-        mesh.joinMesh(backFace, pos.x, pos.y + .5, pos.z + .5);
-        mesh.joinMesh(frontFace, pos.x, pos.y + .5, pos.z - .5);
-
-        // right and Left faces
-        mesh.joinMesh(rightFace, pos.x + .5, pos.y + .5, pos.z);
-        mesh.joinMesh(leftFace, pos.x - .5, pos.y + .5, pos.z);
-        break;
+    int32_t i = 0;
+    for (const vec& direction : allDirections) {
+      const vec adjacentPos = pos + direction;
+      if (chunk.getBlockType(adjacentPos) == Block::Type::AIR) {
+        // Center and then reposition
+        mesh.joinMesh(faces[i], pos.x + (float)direction.x / 2,
+                      pos.y + 0.5 + (float)direction.y / 2,
+                      pos.z + (float)direction.z / 2);
       }
+      i++;
     }
   }
   return mesh;
